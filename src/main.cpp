ccd4dec41185697eca5317b94f1440cc94e0f27f
Javier Lucena <javluccas@gmail.com>
2024-12-09 11:43:24 +0100

    Updated with step005

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,37 @@
-/**
- * This file is part of the "Learn WebGPU for C++" book.
- *   https://github.com/eliemichel/LearnWebGPU
- *
- * MIT License
- * Copyright (c) 2022-2024 Elie Michel
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-// Include WebGPU header
-#include <cassert>
-#include <ios>
-#include <iostream>
-#include <iterator>
-#include <ostream>
-#include <regex>
-#include <vector>
 #include <webgpu/webgpu.h>
 
-#ifdef __EMSCRIPTEM__
+#ifdef __EMSCRIPTEN__
 #include <emscripten.h>
-#endif // __EMSCRIPTEM__
+#endif // __EMSCRIPTEN__
 
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+/**
+ * Utility function to get a WebGPU adapter, so that
+ *     WGPUAdapter adapter = requestAdapterSync(options);
+ * is roughly equivalent to
+ *     const adapter = await navigator.gpu.requestAdapter(options);
+ */
 WGPUAdapter requestAdapterSync(WGPUInstance instance,
                                WGPURequestAdapterOptions const *options) {
+  // A simple structure holding the local information shared with the
+  // onAdapterRequestEnded callback.
   struct UserData {
     WGPUAdapter adapter = nullptr;
     bool requestEnded = false;
   };
-
   UserData userData;
 
+  // Callback called by wgpuInstanceRequestAdapter when the request returns
+  // This is a C++ lambda function, but could be any function defined in the
+  // global scope. It must be non-capturing (the brackets [] are empty) so
+  // that it behaves like a regular C function pointer, which is what
+  // wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
+  // is to convey what we want to capture through the pUserData pointer,
+  // provided as the last argument of wgpuInstanceRequestAdapter and received
+  // by the callback as its last argument.
   auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status,
                                   WGPUAdapter adapter, char const *message,
                                   void *pUserData) {
@@ -54,14 +39,16 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance,
     if (status == WGPURequestAdapterStatus_Success) {
       userData.adapter = adapter;
     } else {
-      std::cout << "Could not get WebGPU adapter" << std::endl;
+      std::cout << "Could not get WebGPU adapter: " << message << std::endl;
     }
     userData.requestEnded = true;
   };
 
-  wgpuInstanceRequestAdapter(instance, options, onAdapterRequestEnded,
-                             (void *)&userData);
+  // Call to the WebGPU request adapter procedure
+  wgpuInstanceRequestAdapter(instance /* equivalent of navigator.gpu */,
+                             options, onAdapterRequestEnded, (void *)&userData);
 
+  // We wait until userData.requestEnded gets true
 #ifdef __EMSCRIPTEN__
   while (!userData.requestEnded) {
     emscripten_sleep(100);
@@ -69,41 +56,11 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance,
 #endif // __EMSCRIPTEN__
 
   assert(userData.requestEnded);
+
   return userData.adapter;
 }
 
-int main(int, char **) {
-  // We create a descriptor
-  WGPUInstanceDescriptor desc = {};
-  desc.nextInChain = nullptr;
-
-  // We create the instance using this descriptor
-#ifdef WEBGPU_BACKEND_EMSCRIPTEN
-  WGPUInstance instance = wgpuCreateInstance(nullptr);
-#else  //  WEBGPU_BACKEND_EMSCRIPTEN
-  WGPUInstance instance = wgpuCreateInstance(&desc);
-#endif //  WEBGPU_BACKEND_EMSCRIPTEN
-
-  // We can check whether there is actually an instance created
-  if (!instance) {
-    std::cerr << "Could not initialize WebGPU!" << std::endl;
-    return 1;
-  }
-
-  // Display the object (WGPUInstance is a simple pointer, it may be
-  // copied around without worrying about its size).
-  std::cout << "WGPU instance: " << instance << std::endl;
-
-  WGPURequestAdapterOptions adapterOpts = {};
-  adapterOpts.nextInChain = nullptr;
-  WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
-
-  std::cout << "Requesting adapter: " << std::endl;
-  std::cout << "Got adapter " << adapter << std::endl;
-
-  // We clean up the WebGPU instance
-  wgpuInstanceRelease(instance);
-
+void inspectAdapter(WGPUAdapter adapter) {
 #ifndef __EMSCRIPTEN__
   WGPUSupportedLimits supportedLimits = {};
   supportedLimits.nextInChain = nullptr;
@@ -113,39 +70,43 @@ int main(int, char **) {
       wgpuAdapterGetLimits(adapter, &supportedLimits) == WGPUStatus_Success;
 #else
   bool success = wgpuAdapterGetLimits(adapter, &supportedLimits);
-#endif // WEBGPU_BACKEND_DAWN
+#endif
+
   if (success) {
     std::cout << "Adapter limits:" << std::endl;
-    std::cout << "-- maxTextureDimension1D: "
+    std::cout << " - maxTextureDimension1D: "
               << supportedLimits.limits.maxTextureDimension1D << std::endl;
-    std::cout << "-- maxTextureDimension2D: "
+    std::cout << " - maxTextureDimension2D: "
               << supportedLimits.limits.maxTextureDimension2D << std::endl;
-    std::cout << "-- maxTextureDimension3D: "
+    std::cout << " - maxTextureDimension3D: "
               << supportedLimits.limits.maxTextureDimension3D << std::endl;
-    std::cout << "-- maxTextureArrayLayers: "
+    std::cout << " - maxTextureArrayLayers: "
               << supportedLimits.limits.maxTextureArrayLayers << std::endl;
   }
-#endif // !__EMSCRIPTEN__
-
+#endif // NOT __EMSCRIPTEN__
   std::vector<WGPUFeatureName> features;
 
+  // Call the function a first time with a null return address, just to get
+  // the entry count.
   size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+  // Allocate memory (could be a new, or a malloc() if this were a C program)
   features.resize(featureCount);
 
+  // Call the function a second time, with a non-null return address
   wgpuAdapterEnumerateFeatures(adapter, features.data());
 
   std::cout << "Adapter features:" << std::endl;
-  std::cout << std::hex;
+  std::cout << std::hex; // Write integers as hexadecimal to ease comparison
+                         // with webgpu.h literals
   for (auto f : features) {
     std::cout << " - 0x" << f << std::endl;
   }
-  std::cout << std::dec;
-
+  std::cout << std::dec; // Restore decimal numbers
   WGPUAdapterProperties properties = {};
   properties.nextInChain = nullptr;
-
   wgpuAdapterGetProperties(adapter, &properties);
-  std::cout << "Adapter properties" << std::endl;
+  std::cout << "Adapter properties:" << std::endl;
   std::cout << " - vendorID: " << properties.vendorID << std::endl;
   if (properties.vendorName) {
     std::cout << " - vendorName: " << properties.vendorName << std::endl;
@@ -161,14 +122,41 @@ int main(int, char **) {
     std::cout << " - driverDescription: " << properties.driverDescription
               << std::endl;
   }
-
   std::cout << std::hex;
-  std::cout << " - adapterType: 0x" << properties.adapterType
-            << std::endl;
-  std::cout << " - backendType: 0x" << properties.backendType
-            << std::endl;
+  std::cout << " - adapterType: 0x" << properties.adapterType << std::endl;
+  std::cout << " - backendType: 0x" << properties.backendType << std::endl;
+  std::cout << std::dec; // Restore decimal numbers
+}
 
-  std::cout << std::dec;
+int main() {
+  WGPUInstanceDescriptor desc = {};
+  desc.nextInChain = nullptr;
+
+#ifdef WEBGPU_BACKEND_EMSCRIPTEN
+  WGPUInstance instance = wgpuCreateInstance(nullptr);
+#else  //  WEBGPU_BACKEND_EMSCRIPTEN
+  WGPUInstance instance = wgpuCreateInstance(&desc);
+#endif //  WEBGPU_BACKEND_EMSCRIPTEN
+
+  if (!instance) {
+    std::cerr << "Could not initialize WebGPU!" << std::endl;
+    return 1;
+  }
+
+  std::cout << "WGPU instance: " << instance << std::endl;
+
+  std::cout << "Requesting adapter..." << std::endl;
+  WGPURequestAdapterOptions adapterOpts = {};
+  adapterOpts.nextInChain = nullptr;
+  WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+  std::cout << "Got adapter: " << adapter << std::endl;
+
+  // Display some information about the adapter
+  inspectAdapter(adapter);
+
+  // We no longer need to use the instance once we have the adapter
+  wgpuInstanceRelease(instance);
+
   wgpuAdapterRelease(adapter);
 
   return 0;
